4e94c7c4c9d904406874966e9bffb4c671548302
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-03-21 16:36:44 -0400

    Updated google sign in button styling and added signin/sign out to header

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react"
 import FormInput from "../form-input/form-input.component"
 import CustomButton from "../custom-button/custom-button.component"
 
-import signInWithGoogle from "../../firebase/firebase.utils"
+import { signInWithGoogle } from "../../firebase/firebase.utils"
 
 import "./sign-in.styles.scss"
 
@@ -25,13 +25,14 @@ class SignIn extends React.Component {
 
   handleChange = event => {
     const { value, name } = event.target
+
     this.setState({ [name]: value })
   }
 
   render() {
     return (
       <div className="sign-in">
-        <h2> I already have an account</h2>
+        <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -46,16 +47,18 @@ class SignIn extends React.Component {
           <FormInput
             name="password"
             type="password"
-            handleChange={this.handleChange}
             value={this.state.password}
+            handleChange={this.handleChange}
             label="password"
             required
           />
-          <CustomButton type="submit">Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            {" "}
-            Sign in with Google{" "}
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {" "}
+              Sign in with Google{" "}
+            </CustomButton>
+          </div>
         </form>
       </div>
     )
