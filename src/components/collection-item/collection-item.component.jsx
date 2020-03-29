addc2c990e6ab4448dbffd03833c79f26313e06e
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-03-29 15:19:45 -0400

    updated shopping cart changes and pulled back the videos to prepare for full shopping cart integration

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,31 +1,34 @@
 import React from "react"
+import { connect } from "react-redux"
 
-import ReactPlayer from "react-player"
+import CustomButton from "../custom-button/custom-button.component"
+import { addItem } from "../../redux/cart/cart.actions"
 
 import "./collection-item.styles.scss"
 
-const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-
-const CollectionItem = ({ id, name, price, videoUrl }) => (
-  <div className="collection-item">
-    <ReactPlayer
-      className="react-player"
-      url={videoUrl}
-      controls
-      width="100%"
-      height="100%"
-      config={{
-        file: {
-          forceHLS: !isSafari,
-          forceVideo: true,
-        },
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
-  </div>
-)
+  )
+}
 
-export default CollectionItem
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem)
