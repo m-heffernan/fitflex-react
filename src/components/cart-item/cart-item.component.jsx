a1136498ec37c73ddac6ed020869d45a534db25e
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-04-08 20:56:56 -0400

    added ability to remove an item from the cart in the cart dropdown

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,15 +1,24 @@
 import React from "react"
+import { connect } from "react-redux"
 
 import "./cart-item.styles.scss"
+import { clearItemFromCart } from "../../redux/cart/cart.actions"
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+const CartItem = ({ item: { imageUrl, price, name }, item, clearItem }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item" />
     <div className="item-details">
       <span className="name">{name}</span>
       <span className="price">${price}</span>
     </div>
+    <div className="remove-button" onClick={() => clearItem(item)}>
+      &#10005;
+    </div>
   </div>
 )
 
-export default CartItem
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+})
+
+export default connect(null, mapDispatchToProps)(CartItem)
