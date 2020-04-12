5a77c4ff49fe146f419cf2fed6d3298306af9519
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-04-12 09:41:06 -0400

    added loading spinner and fixed h1 on hero

--- src/pages/videos/video.component.jsx
+++ src/pages/videos/video.component.jsx
@@ -12,7 +12,16 @@ import {
 
 import { updateCollections } from "../../redux/shop/shop.actions"
 
+import WithSpinner from "../../components/with-spinner/with-spinner.component"
+
+const VideoOverviewWithSpinner = WithSpinner(VideoCollectionsOverview)
+const VideoPageWithSpinner = WithSpinner(VideoCollectionPage)
+
 class VideoPage extends React.Component {
+  state = {
+    loading: true,
+  }
+
   unsubscribeFromSnapshot = null
 
   componentDidMount() {
@@ -28,17 +37,22 @@ class VideoPage extends React.Component {
 
   render() {
     const { match } = this.props
+    const { loading } = this.state
 
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
-          component={VideoCollectionsOverview}
+          render={props => (
+            <VideoOverviewWithSpinner isLoading={loading} {...props} />
+          )}
         />
         <Route
           path={`${match.path}/:collectionId`}
-          component={VideoCollectionPage}
+          render={props => (
+            <VideoPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     )
